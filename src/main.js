9ae7e02777a5cb93f635165f6ad96797991e2d23
VinT04 <tyagivinamra@gmail.com>
2024-11-08 22:24:26 -0500

    adding new mongo stuff

--- src/main.js
+++ src/main.js
@@ -1,30 +1,25 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://ssheedy:WZqgjWWHnZU4BsTl@vsv.wcf72.mongodb.net/?retryWrites=true&w=majority&appName=VSV";
+const { MongoClient } = require("mongodb");
+import { Grid } from './grid'
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
 async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
+    const uri =
+        "mongodb+srv://ssheedy:WZqgjWWHnZU4BsTl@vsv.wcf72.mongodb.net/?retryWrites=true&w=majority";
+
+
+    const client = new MongoClient(uri);
+
+
+    await client.connect();
+
+    const dbName = "myDatabase";
+    const collectionName = "recipes";
+
+    const database = client.db(dbName);
+    const collection = database.collection(collectionName);
 }
-run().catch(console.dir);
 
 let g = new Grid()
 
 const grid_element = document.getElementById('grid1')
 
-g.render(grid_element)
+g.render(grid_element);
