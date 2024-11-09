d99d4b09877f24b26c4834e0aaa38024b00bff2f
sam-s04 <sam.sheedy4@gmail.com>
2024-11-08 23:03:49 -0500

    edits

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import { Grid } from './src/grid.js';
 
-let g = new Grid()
-g.newGrid(5, 5)
+let g = new Grid();
+g.newGrid(5, 5);
 
-const grid_element = document.getElementById('grid1')
+const grid_element = document.getElementById('grid1');
 
 g.render(grid_element);
