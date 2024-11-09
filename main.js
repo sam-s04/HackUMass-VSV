a45fdcffe330568e4b9ce0eec9483c2a21cc616e
sam-s04 <sam.sheedy4@gmail.com>
2024-11-09 13:20:58 -0500

    cute happy colors

--- main.js
+++ main.js
@@ -9,8 +9,25 @@ g2.newGrid(5, 5);
 const grid_element1 = document.getElementById('grid1');
 const grid_element2 = document.getElementById('grid2');
 
+const color_circle1 = document.getElementById('color1');
+const color_circle2 = document.getElementById('color2');
+const color_circle3 = document.getElementById('color3');
+const color_circle4 = document.getElementById('color4');
+
+let color_circle_list = [];
+color_circle_list.push(color_circle1);
+color_circle_list.push(color_circle2);
+color_circle_list.push(color_circle3);
+color_circle_list.push(color_circle4);
+
 g1.render(grid_element1);
 g2.render(grid_element2);
+
+let color_list = ['#689942', '#6D5995', '#00527A', '#E75480']
+
+for(let i in color_circle_list){
+  color_circle_list[i].style.backgroundColor = color_list[i];
+}
 
 function paint_pixel(pixel) {
   let color = document.getElementById('paintbrush').value;
