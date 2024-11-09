5438f1a9eb9c78e70225d83486b6de1ba6f9009c
sam-s04 <sam.sheedy4@gmail.com>
2024-11-08 22:01:25 -0500

    mongo shit

---
+++ grid.js
@@ -0,0 +1,69 @@
+export class Grid {
+    constructor(){
+      this.grid = []
+    }
+  
+    getGrid(){
+      return this.grid;
+    }
+  
+    setGrid(grid){
+      this.grid = grid;
+    }
+  
+    newGrid(height, width){
+      this.grid = [];
+  
+      for(let i = 0; i < height; i++){
+        let row = []
+        for(let j = 0; j < width; j++){
+          row.push(null);
+        }
+        this.grid.push(row);
+      }
+    }
+  
+    render(element){
+      element.innerHTML = '';
+      let height = this.grid.length;
+      let width = this.grid[0].length;
+      let scale = 20;
+      element.innerHTML = `<style id="style">
+        #grid {
+          display: grid;
+          margin: auto;
+          grid-template-columns: repeat(${width}, auto);
+          height: ${height*scale}px;
+          width: ${width*scale}px;
+          margin-bottom: 3em;
+        }
+        </style>`;
+  
+      for(let i = 0; i < height; i++){
+        for(let j = 0; j < width; j++){
+          let new_pixel = document.createElement('div');
+          new_pixel.classList.add('pixel');
+          if(height <= 15 && width <= 15){
+            new_pixel.classList.add('standard');
+          }
+          else{
+            new_pixel.classList.add('small');
+          }
+          new_pixel.classList.add(`[${i},${j}]`);
+          let color = this.grid[i][j];
+          this.colorPixel(new_pixel, color);
+          element.appendChild(new_pixel);
+        }
+      }
+    }
+  
+    colorPixel(element, color){
+      let pos = JSON.parse(element.classList[2]); // example class list: pixel standard [1,1]
+      this.grid[pos[0]][pos[1]] = color;
+      element.innerHTML = `<style>
+        .\\[${pos[0]}\\,${pos[1]}\\] {
+          background-color: #${color};
+        }
+        </style>`;
+    }
+  }
---
+++ main.js
@@ -0,0 +1,5 @@
+let g = new Grid()
+
+const grid_element = document.getElementById('grid1')
+
+g.render(grid_element)
