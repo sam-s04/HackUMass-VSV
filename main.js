6592a52efb89b794c31f8e30e42d08ca51228d54
vc64 <vichen2004@gmail.com>
2024-11-09 12:51:22 -0500

    add drag and draw functionality

--- main.js
+++ main.js
@@ -12,15 +12,30 @@ const grid_element2 = document.getElementById('grid2');
 g1.render(grid_element1);
 g2.render(grid_element2);
 
-function activate_pixels(){
-    let pixels = document.getElementsByClassName('2');
-    for (let i = 0; i < pixels.length; i++) {
-      pixels[i].addEventListener('click', () => {
-        let color = document.getElementById('paintbrush').value;
-        color = color.replace('#', '');
-        g2.colorPixel(pixels[i], color);
-      });
-    }
-  }
+function paint_pixel(pixel) {
+  let color = document.getElementById('paintbrush').value;
+  color = color.replace('#', '');
+  g2.colorPixel(pixel, color);
+}
 
-activate_pixels()
+function activate_pixels() {
+  const pixels = Array.from(document.getElementsByClassName('2'));
+  const pixel_event_funcs = [];
+  pixels.forEach(pixel => {
+    const paint_curr_pixel = () => paint_pixel(pixel);
+    pixel_event_funcs.push(paint_curr_pixel);
+    pixel.addEventListener('mousedown', paint_curr_pixel);
+  });
+
+  grid_element2.addEventListener("mousedown", () => {
+    pixels.forEach((pixel, idx) => {
+      pixel.addEventListener("mouseover", pixel_event_funcs[idx]);
+    });
+  });
+
+  grid_element2.addEventListener("mouseup", () => {
+    pixels.forEach((pixel, idx) => pixel.removeEventListener("mouseover", pixel_event_funcs[idx]));
+  });
+}
+
+activate_pixels();
